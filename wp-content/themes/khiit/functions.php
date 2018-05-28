1ecb90a3e38a3198011e5e07029041ae179e51f0
alexeyEP <alexey.nikitenko@easternpeak.com>
2018-05-28 18:46:19 +0300

    full page

--- wp-content/themes/khiit/functions.php
+++ wp-content/themes/khiit/functions.php
@@ -45,6 +45,7 @@ if ( ! function_exists( 'khiit_setup' ) ) :
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
 			'main-menu' => esc_html__( 'Primary', 'khiit' ),
+            'fullpage-menu' => esc_html__( 'Fullpage', 'khiit' )
 		) );
 
 		/*
--- wp-content/themes/khiit/page-templates/second.php
+++ wp-content/themes/khiit/page-templates/second.php
@@ -5,35 +5,56 @@ Template Name: Second template
 get_header(); ?>
 
 <?php
-    $args = array( 'post_type' => 'our_story' );
-    $loop = new WP_Query( $args );
 
-echo '<div class="fullpage">';
-    while ( $loop->have_posts() ) : $loop->the_post();
-        echo '<section class="section">';
-        ?>
-        <?php
+    $slug = basename(get_permalink());
+    $args = array( 'post_type' => $slug );
+    $query = new WP_Query( $args );
+
+    $posts = $query->posts;
+    uasort($posts, function ($a, $b) {
+        $aOrder = get_field('order', $a->ID);
+        $bOrder = get_field('order', $b->ID);
+
+        if ($aOrder == $bOrder) {
+            return 0;
+        }
+
+        return ($aOrder < $bOrder) ? -1 : 1;
+    });
+
+    echo '<div class="fullpage">';
+        foreach ($posts as $post) {
+            setup_postdata($post);
+
+
+            echo '<section class="section" id="' . get_post_type($post) . '_' . strtolower(str_replace(' ', '_', the_title('', '', false))) . '">';
+
+            ?>
+
+            <?php
             $image = get_field('image');
             $heading = get_field('heading');
-            $title = trim(the_title());
-        ?>
+
+            ?>
 
             <div class="content-col">
-                <?php echo 'our_story_'.$title ?>
                 <strong class="ttl"><?php echo the_title(); ?></strong>
                 <h1 class="heading with-separator"><?php echo $heading; ?></h1>
                 <?php echo the_content(); ?>
+
+
                 <a href="#" class="lnk-next">arrow-down</a>
             </div>
             <div class="aside-img">
-                <img src="<?php echo $image['url']; ?>" alt="" /><?php /*echo $image['alt'] */?>
+                <img src="<?php echo $image['url']; ?>" alt="" />
             </div>
 
-        <?php
+            <?php
             wp_reset_postdata();
             echo '</section>';
-    endwhile;
-echo '</fullpage>';
+        }
+
+    echo '</fullpage>';
 
 ?>
 
